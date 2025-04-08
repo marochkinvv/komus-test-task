ff2417bac7600c7c105fddddb8c4239c1a784dcf
marochkinvv <marochkinvv@gmail.com>
2025-04-08 08:37:11 +0300

    Initial commit

---
+++ src/linksData.js
@@ -0,0 +1,18 @@
+const linksData = [
+  {
+    link: 'https://www.komus.ru',
+    title: 'Главная Комус',
+  },
+  {
+    link: 'https://www.komus.ru/store/',
+    title: 'Магазины Комус',
+  },
+  {
+    link: 'https://www.komus.ru/cart/',
+    title: 'Корзина Комус',
+  },
+  {
+    link: 'https://www.komus.ru/news/',
+    title: 'Новости Комус',
+  },
+];
